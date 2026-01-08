2db6ad2e805458afa570f4ac3ea74455576cd5da
Kiril Stavrev <kstavreb@gmail.com>
2026-01-08 10:28:33 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+import joblib
+import requests
+import io
+
+st.set_page_config(page_title="Handwritten Digit Recognition")
+st.tilte("Handwrittem Digit Recognition")
+st.write("Upload a handwritten digit image and AI will try to recognize it.")
+
+@st.cache_resource
+def load_model():
+  try:
+    from sklearn.datasets import load_digits
+    from sklearn.nerual_network import MLPClassifier
+    from sklearn.model_selection import train_test_split
+
+    digit = load_digit()
+    X = digits.images.reshape((len(digit.images), -1)) / 16.0
+    y = digits.target
+    X_train, _, y_train, _ = train_test_split(X, y, test_size=0.2, random_state=42)
